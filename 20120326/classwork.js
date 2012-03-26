2f4aa674f060dc7a108b4d954cec844fba42f90d
Dario <pasquantonio.d@gmail.com>
2012-03-26 16:52:19 +0200

    ciao

--- 20120326/classwork.js
+++ 20120326/classwork.js
@@ -1,8 +1,29 @@
 var monodom = DOMAIN([[3,5]])([3])
-DRAW(monodom)
+DRAW(monodom);
 
 var bidom = DOMAIN([[3,5],[4,5]])([3,3])
-DRAW(bidom)
+DRAW(bidom);
 
 var tridom = DOMAIN([[3,5],[4,5],[0,1]])([3,3,2])
-DRAW(tridom)
+DRAW(tridom);
+
+var domain = DOMAIN([[0,10]])([10]);
+
+var mapping = function(array){
+var u = array[0];
+
+return [u,1];
+};
+
+var mapped = MAP(mapping)(domain);
+
+
+var domBise = DOMAIN([[0,10]])([10]);
+
+var mappingBis = function(array){
+	var u = array[0];
+
+	return [u,u];
+};
+
+var mappedBis = MAP(mappingBis)(domBise)
