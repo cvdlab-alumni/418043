e61dfb1a7552b14be9d1391f10e3c5492e038c7c
Dario <pasquantonio.d@gmail.com>
2012-03-26 17:09:17 +0200

    ciao

--- 20120326/classwork.js
+++ 20120326/classwork.js
@@ -7,6 +7,8 @@ DRAW(bidom);
 var tridom = DOMAIN([[3,5],[4,5],[0,1]])([3,3,2])
 DRAW(tridom);
 
+//retta traslata di 1
+
 var domain = DOMAIN([[0,10]])([10]);
 
 var mapping = function(array){
@@ -17,6 +19,7 @@ return [u,1];
 
 var mapped = MAP(mapping)(domain);
 
+//bisettrice
 
 var domBise = DOMAIN([[0,10]])([10]);
 
@@ -26,4 +29,39 @@ var mappingBis = function(array){
 	return [u,u];
 };
 
-var mappedBis = MAP(mappingBis)(domBise)
+var mappedBis = MAP(mappingBis)(domBise);
+
+DRAW(mappedBis);
+
+//sinusoide
+
+var domSinus = DOMAIN([[0,2*Math.PI]])([10]);
+
+var mappingSin = function(array){
+var u = array[0];
+
+return [u, SIN(u)];
+}
+var mappedSin = MAP(mappingSin)(domSinus);
+
+DRAW(mappedSin);
+
+//circonferenza
+
+
+var drawCircle = function(r, n){
+var domCircle = DOMAIN([[0,2*Math.PI]])([n]);
+var mappingCircle = function(array){
+	var u = array[0];
+	var x = Math.cos(u);
+	var y = Math.sin(u);
+
+	return [r*x, r*y];
+}
+
+var mappedCir = MAP(mappingCircle)(domCircle);
+
+DRAW(mappedCir);
+}
+
+
