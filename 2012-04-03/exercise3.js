368c7a00187d2ae38647bc89b7bfe753c3fedf0a
Dario <pasquantonio.d@gmail.com>
2012-04-04 15:29:14 +0200

    ex

--- 2012-04-03/exercise3.js
+++ 2012-04-03/exercise3.js
@@ -19,6 +19,7 @@ floor.push(SIMPLEX_GRID([[-47, 4.2],[-16, 0.2],[hBase]]));
 floor.push(SIMPLEX_GRID([[-51, 0.2],[-6, 10],[hBase]]));
 floor.push(SIMPLEX_GRID([[-36, 3],[-16, 1],[hBase]]));
 var floorStruct = STRUCT(floor);
+COLOR([215/255,201/255,177/250])(floorStruct);
 DRAW(floorStruct);
 
 var walls = [];
@@ -35,6 +36,7 @@ walls.push(SIMPLEX_GRID([[-41.4, 9.8],[-4.8, 0.2],[-hBase, hWall]]));
 walls.push(SIMPLEX_GRID([[-1,7],[-0.8,0.2],[-hBase, hWall]]));
 
 var wallStruct = STRUCT(walls);
+COLOR([215/255,201/255,177/250])(wallStruct);
 DRAW(wallStruct);
 
 var minorWalls = [];
@@ -47,6 +49,7 @@ minorWalls.push(SIMPLEX_GRID([[-7, 2],[-20.8, 0.1],[-hBase, hMinorWall]]));
 //minorWalls.push(SIMPLEX_GRID([[-6.95, 0.1],[-20.9, 1.1],[-hBase, hMinorWall]]));
 
 var minorWallStruct = STRUCT(minorWalls);
+COLOR([0,0,0])(minorWallStruct);
 DRAW(minorWallStruct);
 
 var glasses = [];
@@ -58,6 +61,7 @@ glasses.push(SIMPLEX_GRID([[-31.95,0.05],[-7.5, 6.1],[-hBase, hMinorWall]]));
 glasses.push(SIMPLEX_GRID([[-44.7,0.05],[-6.9, 7.2],[-hBase, hMinorWall]]));
 
 var glassesStruct = STRUCT(glasses);
+COLOR([175/255, 239/255, 244/255])(glassesStruct);
 DRAW(glassesStruct);
 
 // scalini
@@ -69,6 +73,7 @@ h-=2/8
 };
 
 var stepsStruct = STRUCT(steps);
+COLOR([215/255,201/255,177/250])(stepsStruct);
 DRAW(stepsStruct);
 
 //columns
@@ -83,15 +88,22 @@ columns.push(T([0,1])([38.5, 7])(column));
 columns.push(T([0,1])([45, 14])(column));
 columns.push(T([0,1])([45, 7])(column));
 var columnsStruct = STRUCT(columns);
+COLOR([0,0,0])(columnsStruct);
 DRAW(columnsStruct);
 
-
+var pools = [];
+pools.push(SIMPLEX_GRID([[-1, 20],[-1, 9],[-1.8, 0.2]]));
+pools.push(SIMPLEX_GRID([[-47, 4],[-5, 11],[-1.8, 0.2]]));
+var poolsStruct = STRUCT(pools);
+COLOR([98/255,203/255,211/255])(poolsStruct);
+DRAW(poolsStruct);
 
 // reefs
 var reefs = [];
 reefs.push(SIMPLEX_GRID([[-0.3, 8.8 + 0.7],[-13.2, 9.8],[-hBase - hWall, hReef]]));
 reefs.push(SIMPLEX_GRID([[-24,23],[-4, 13],[-hBase - hWall, hReef]]));
 var reefStruct = STRUCT(reefs);
+COLOR([135/255, 47/255, 30/255])(reefStruct);
 DRAW(reefStruct);
 
 // bench
