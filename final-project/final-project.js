d30efe57e39a0aaa91fd67e32b08a3dccb778976
Dario Pasquantonio <dario@Dario-Mac.local>
2012-07-18 12:25:52 +0200

    fix

--- final-project/final-project.js
+++ final-project/final-project.js
@@ -915,10 +915,10 @@
 		var points = rotatePoints(pointsNR,-PI/4,1);
 		var points2 = rotatePoints(points,-PI/2,1);
 
-		points = translatePoints(points,2,zGableBase);
-		points = translatePoints(points,0,-3.85);
-		points2 = translatePoints(points2,0,xGableBase-0.2+4.06);
-		points2 = translatePoints(points2,2,zGableBase);
+		points = translatePoints(points,2,zGableBase+0.02);
+		points = translatePoints(points,0,-3.82);
+		points2 = translatePoints(points2,0,xGableBase-0.22+4.06);
+		points2 = translatePoints(points2,2,zGableBase+0.02);
 		var knots = makeKnots(points);
 		var profile = NUBS(S0)(2)(knots)(points);
 		var profile2 = NUBS(S0)(2)(knots)(points2);
@@ -1485,7 +1485,7 @@
 
 	//builds the roof, a part of the finalRoof
 	var buildRoof1 = function(){
-		var lRoof = 16.03;
+		var lRoof = 16.06;
 		var hRoof = 4;
 		var bRoofPoints = [[0,0,0],[0,0,0],[lRoof,0,0],[lRoof,0,0],[lRoof,0,lRoof],[lRoof,0,lRoof],[0,0,lRoof],[0,0,lRoof],[0,0,0],[0,0,0]];
 		var bRoofdepth = NUBS(S0)(1)([0,0,1,2,3,4,5,6,7,10,11,11])(bRoofPoints);
@@ -1619,7 +1619,7 @@
 		var chimney3 = T([2])([15.35])(chimney);
 		var chimney4 = T([2])([15.35])(chimney2);
 		var roof = STRUCT([roof,dome,chimney,chimney2,chimney3,chimney4]);
-		roof = T([0,1,2])([-3.8,11.17,6.7])(roof);
+		roof = T([0,1,2])([-3.81,11.16,6.69])(roof);
 		return roof;
 	}
 	
@@ -1687,8 +1687,8 @@
 
 //builds the entire ville, using the three functions those generates the three macro-blocks
 var ville = [];
-var facades = buildFacades();
-ville.push(facades);
+//var facades = buildFacades();
+//ville.push(facades);
 var walls = buildWalls();
 ville.push(walls);
 var roof = buildRoof();
